bed4336a4468b1808081f81f9a5cc47582250945
gebhartn <nicholas.gebhart@gmail.com>
2020-04-09 11:48:23 -0700

    chore: updates siteconfig

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -2,19 +2,28 @@ module.exports = {
   siteTitle: 'Nicholas Gebhart',
   siteTitleShort: 'N. Gebhart',
   siteTitleAlt: 'Nicholas Gebhart',
+  siteDescriptionAlt: 'Full Stack software developer',
   siteDescription: 'Nicholas Gebhart is a full stack software developer',
   links: [
     {
+      name: 'Home',
+      to: '/',
+      title: 'Home',
+    },
+    {
       name: 'About',
       to: '/me/',
+      title: 'Who?',
     },
     {
       name: 'Blog',
       to: '/blog/',
+      title: 'What?',
     },
     {
       name: 'Contact',
       to: '/contact/',
+      title: 'Where?',
     },
   ],
 }
