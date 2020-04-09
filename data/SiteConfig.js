62a21b4dfc420e9f54f28bc37ffbc331c678fb0f
gebhartn <nicholas.gebhart@gmail.com>
2020-04-08 23:36:16 -0700

    chore: adds siteconfig file

---
+++ data/SiteConfig.js
@@ -0,0 +1,20 @@
+module.exports = {
+  siteTitle: 'Nicholas Gebhart',
+  siteTitleShort: 'N. Gebhart',
+  siteTitleAlt: 'Nicholas Gebhart',
+  siteDescription: 'Nicholas Gebhart is a full stack software developer',
+  links: [
+    {
+      name: 'About me',
+      to: '/me/',
+    },
+    {
+      name: 'Blog',
+      to: '/blog/',
+    },
+    {
+      name: 'Contact',
+      to: '/contact/',
+    },
+  ],
+}
