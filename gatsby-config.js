174c19a6b262bfafcf931463437f545930a0915d
gebhartn <nicholas.gebhart@gmail.com>
2020-04-09 05:56:32 -0700

    feat: serving markdown pages!

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,16 @@ module.exports = {
     description: `Nicholas Gebhart is a full stack software developer`,
     author: `Nicholas Gebhart`,
   },
-  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/content/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 }
