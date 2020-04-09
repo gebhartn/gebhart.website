5732eca29d16198397e61b7ca880f900ba44383e
gebhartn <nicholas.gebhart@gmail.com>
2020-04-08 23:36:43 -0700

    chore: adds layout component

---
+++ src/layout/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import Navigation from '../components/Navigation'
+import Footer from '../components/Footer'
+import config from '../../data/SiteConfig'
+import favicon from '../images/favicon.webp'
+import { container, content } from './layout.module.scss'
+
+export default ({ children }) => [
+  <Helmet>
+    <meta name="description" content={config.siteDescription} />
+    <link rel="shortcut icon" type="image/png" href={favicon} />
+  </Helmet>,
+  <div className={container}>
+    <div className={content}>
+      <Navigation />
+      <main id="main">{children}</main>
+    </div>
+    <Footer />
+  </div>,
+]
