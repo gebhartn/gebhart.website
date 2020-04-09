0bf75b3f9009ce26754f8804ea7cf0997868e3c7
gebhartn <nicholas.gebhart@gmail.com>
2020-04-09 05:05:14 -0700

    feat: nav cluster

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,3 +1,47 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import config from '../../../data/SiteConfig'
+import icon from '../../images/favicon.webp'
+import {
+  nav,
+  navContainer,
+  brand,
+  favicon,
+  text,
+  links,
+  cta,
+  scroll,
+} from './navigation.module.scss'
 
-export default () => 'Hello World'
+export default () => {
+  const [scrolled, setScrolled] = React.useState(false)
+
+  const handleScroll = () =>
+    window.scrollY > 20 ? setScrolled(true) : setScrolled(false)
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  })
+
+  return (
+    <nav className={scrolled ? `${nav} ${scroll}` : nav}>
+      <div className={navContainer}>
+        <div className={brand}>
+          <Link to="/">
+            <img src={icon} className={favicon} alt="Computer" />
+            <span className={text}>{config.siteTitle}</span>
+          </Link>
+        </div>
+        <div className={links}>
+          {config.links.map(({ name, to }) => (
+            <Link key={name} to={to}>
+              {name}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </nav>
+  )
+}
