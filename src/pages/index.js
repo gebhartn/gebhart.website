26c774a2911339931c3ed234dbe9277d48197a09
gebhartn <nicholas.gebhart@gmail.com>
2020-04-09 11:49:18 -0700

    styles: icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,16 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../layout'
 import SEO from '../components/SEO'
+import Hero from '../components/Hero'
 import config from '../../data/SiteConfig'
+import { container } from './index.module.scss'
 
 export default () => (
   <Layout>
     <SEO />
     <Helmet title={`${config.siteTitle} - Full Stack Software Developer`} />
-    <div style={{ height: '2000px' }}>Hello World!</div>
+    <div className={container}>
+      <Hero />
+    </div>
   </Layout>
 )
