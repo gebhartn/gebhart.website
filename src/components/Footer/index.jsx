8e9013af4f29811fd372f343db375744ab814309
gebhartn <nicholas.gebhart@gmail.com>
2020-04-09 11:48:32 -0700

    chore: adds basic footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,3 +1,39 @@
 import React from 'react'
+import github from '../../images/github.png'
+import netlify from '../../images/netlify.svg'
+import gatsby from '../../images/gatsby.svg'
+import { footer, logo, logoContainer } from './footer.module.scss'
 
-export default () => 'Hello World'
+export default () => (
+  <footer className={footer}>
+    <div className={logoContainer}>
+      <a href="https://github.com/gebhartn" title="Check out my Github">
+        <img
+          src={github}
+          alt="Github Logo"
+          className={logo}
+          target="_blank"
+          rel="noopener noreferrer"
+        />
+      </a>
+      <a href="https://www.gatsbyjs.org" title="Powered by Gatsby">
+        <img
+          src={gatsby}
+          alt="Github Logo"
+          className={logo}
+          target="_blank"
+          rel="noopener noreferrer"
+        />
+      </a>
+      <a href="https://www.netlify.com" title="Distributed by Netlify">
+        <img
+          src={netlify}
+          alt="Github Logo"
+          className={logo}
+          target="_blank"
+          rel="noopener noreferrer"
+        />
+      </a>
+    </div>
+  </footer>
+)
