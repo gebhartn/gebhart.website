3356af09f3954de6e3b8e20301ee98968ba4223e
gebhartn <nicholas.gebhart@gmail.com>
2020-04-10 14:20:47 -0700

    chore: updates static pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,25 @@ import Helmet from 'react-helmet'
 import Layout from '../layout'
 import SEO from '../components/SEO'
 import Hero from '../components/Hero'
-import config from '../../data/SiteConfig'
-import { container } from './index.module.scss'
+import Card from '../components/Card'
+import { siteTitle } from '../../data/SiteConfig'
+import projects from '../../data/projects'
+import { container, section } from './index.module.scss'
 
 export default () => (
   <Layout>
     <SEO />
-    <Helmet title={`${config.siteTitle} - Full Stack Software Developer`} />
+    <Helmet title={`${siteTitle} - Full Stack Software Developer`} />
     <div className={container}>
       <Hero />
+      <section className={section}>
+        <h2>Recent Projects</h2>
+        <Card content={projects} />
+      </section>
+      <section className={section}>
+        <h2>Recent Articles</h2>
+        <Card content={projects} />
+      </section>
     </div>
   </Layout>
 )
