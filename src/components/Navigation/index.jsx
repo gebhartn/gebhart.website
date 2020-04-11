7ddfe0a6de313cbd4ca346d0be981a36a2d4d2a2
gebhartn <nicholas.gebhart@gmail.com>
2020-04-11 08:41:02 -0700

    style: adds horizonal scroll

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { links as navLinks } from '../../../data/SiteConfig'
-import { nav, navContainer, links, scroll } from './navigation.module.scss'
+import {
+  nav,
+  navContainer,
+  links,
+  scroll,
+  text,
+  brand,
+  progressBar,
+  progressContainer,
+} from './navigation.module.scss'
 
 export default () => {
+  const path = window.location.pathname
   const [scrolled, setScrolled] = React.useState(false)
 
-  const handleScroll = () =>
-    window.scrollY > 20 ? setScrolled(true) : setScrolled(false)
+  const scrollBar = () => {
+    const { body, documentElement } = document
+    const windowScroll = body.scrollTop || documentElement.scrollTop
+    const height = documentElement.scrollHeight - documentElement.clientHeight
+    const isScrolled = (windowScroll / height) * 100
+    document.getElementById('bar').style.width = `${isScrolled}%`
+  }
+
+  const handleScroll = () => {
+    if (window.scrollY > 20) setScrolled(true)
+    else setScrolled(false)
+    scrollBar()
+  }
 
   React.useEffect(() => {
     window.addEventListener('scroll', handleScroll)
@@ -16,16 +37,32 @@ export default () => {
   })
 
   return (
-    <nav className={scrolled ? `${nav} ${scroll}` : nav}>
-      <div className={navContainer}>
-        <div className={links}>
-          {navLinks.map(({ title, name, to }) => (
-            <Link title={title} key={name} to={to}>
-              {name}
-            </Link>
-          ))}
+    <>
+      <nav className={scrolled ? `${nav} ${scroll}` : nav}>
+        <div className={navContainer}>
+          {path !== '/' ? (
+            <div className={brand}>
+              <Link to="/">
+                <span className={text}>Nicholas Gebhart</span>
+              </Link>
+            </div>
+          ) : null}
+          <div className={links}>
+            {navLinks.map(({ title, name, to }) => (
+              <Link title={title} key={name} to={to}>
+                {name}
+              </Link>
+            ))}
+          </div>
         </div>
-      </div>
-    </nav>
+        <div
+          className={
+            scrolled ? `${progressContainer} ${scroll}` : progressContainer
+          }
+        >
+          <div className={progressBar} id="bar" />
+        </div>
+      </nav>
+    </>
   )
 }
