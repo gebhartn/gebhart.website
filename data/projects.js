2dbd7737bc7e343044b171b9e23f047a0524c687
gebhartn <nicholas.gebhart@gmail.com>
2020-04-11 10:12:45 -0700

    chore: update structure

--- data/projects.js
+++ data/projects.js
@@ -1,20 +1,31 @@
 export default [
   {
-    name: 'Project One',
-    url: 'https://url.comj',
-    github: 'https://github.ur.com',
-    description: 'A project of one thing',
+    title: 'Mission Control',
+    source: 'https://github.com/Lambda-School-Labs/mission-control-fe',
+    path: 'stage.missionctrl.dev',
+    description: 'Project management anayltics & dashboard',
   },
   {
-    name: 'Project Two',
-    url: 'https://url.comj',
-    github: 'https://github.ur.com',
-    description: 'A project of one thing',
+    title: 'LS-8',
+    source:
+      'https://github.com/Nicholas-Gebhart-Lambda-School/Sprint-Challenge--Computer-Architecture',
+    description: 'An 8-bit computer emulator',
   },
   {
-    name: 'Project Three',
-    url: 'https://url.comj',
-    github: 'https://github.ur.com',
-    description: 'A project of one thing',
+    title: 'Safe Routes',
+    source: '',
+    path: '',
+    description: 'Car crash statistics & dashboard',
+  },
+  {
+    title: 'Salty Hacker',
+    source: '',
+    path: '',
+    description: 'Sentiment analysis of hackernews comments',
+  },
+  {
+    title: 'Werewolf',
+    source: 'https://url.comj',
+    description: 'Discord bot to host a fun party game',
   },
 ]
