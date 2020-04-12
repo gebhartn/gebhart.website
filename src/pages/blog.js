746c17fb9d3ef8fe0c33ad9445c82f735b0bf077
gebhartn <nicholas.gebhart@gmail.com>
2020-04-11 22:24:25 -0700

    chore: blog query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,18 +1,50 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import { graphql, Link } from 'gatsby'
 import Layout from '../layout'
 import SEO from '../components/SEO'
-import Hero from '../components/Hero'
 import { siteTitle } from '../../data/SiteConfig'
-import projects from '../../data/projects'
-import { container, section } from './index.module.scss'
 
-export default () => (
-  <Layout>
-    <SEO />
-    <Helmet title={`${siteTitle} - Full Stack Software Developer`} />
-    <div className={container}>
-      <Hero />
-    </div>
-  </Layout>
-)
+export default ({ data }) => {
+  const { allMarkdownRemark } = data
+  const { edges } = allMarkdownRemark
+
+  return (
+    <Layout>
+      <SEO />
+      <Helmet title={`${siteTitle} - Full Stack Software Developer`} />
+
+      {edges.map(({ node }) => {
+        const { timeToRead, frontmatter } = node
+        return (
+          <div key={frontmatter.title}>
+            <Link to={frontmatter.path}>{frontmatter.title}</Link>
+            <h2>
+              {frontmatter.date} {timeToRead}
+            </h2>
+          </div>
+        )
+      })}
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(
+      sort: { fields: frontmatter___date, order: DESC }
+      filter: { frontmatter: { template: { eq: "post" } } }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+            date
+          }
+          timeToRead
+        }
+      }
+    }
+  }
+`
