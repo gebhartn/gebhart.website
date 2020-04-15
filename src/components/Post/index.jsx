fcdd19adf0d1ab9532fade4634b44b687b79c17c
gebhartn <nicholas.gebhart@gmail.com>
2020-04-14 19:57:55 -0700

    chore: add date to post

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { projects, each, title, buttons, button } from './post.module.scss'
+import formatDate from '../../utils'
+import {
+  projects,
+  each,
+  title,
+  buttons,
+  button,
+  small,
+} from './post.module.scss'
 
 export default ({ posts }) => {
   return (
     <section className={projects}>
-      {posts.map(({ node: { frontmatter, timeToRead } }) => (
-        <Link to={frontmatter.path} className={each} key={frontmatter.title}>
-          <h2>
-            <div className={title}>{frontmatter.title}</div>
-          </h2>
-          <div className={buttons}>
-            <span className={button}>{timeToRead} min read</span>
-          </div>
-        </Link>
-      ))}
+      {posts.map(({ node: { frontmatter, timeToRead } }) => {
+        const date = formatDate(frontmatter.date)
+
+        return (
+          <Link to={frontmatter.path} className={each} key={frontmatter.title}>
+            <h2>
+              <div className={title}>{frontmatter.title}</div>
+              <small className={small}>{date}</small>
+            </h2>
+            <div className={buttons}>
+              <span className={button}>{timeToRead} min read</span>
+            </div>
+          </Link>
+        )
+      })}
     </section>
   )
 }
