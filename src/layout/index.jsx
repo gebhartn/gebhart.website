100edc4657ca73028604559b7c15cc557fe75248
gebhartn <nicholas.gebhart@gmail.com>
2020-04-16 18:48:33 -0700

    chore: seo

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import SEO from '../components/SEO'
 import Navigation from '../components/Navigation'
 import Footer from '../components/Footer'
 import config from '../../data/SiteConfig'
@@ -8,10 +9,11 @@ import { container, content } from './layout.module.scss'
 
 export default ({ children }) => (
   <>
+    <SEO />
     <Helmet>
+      <html lang="en" />
       <meta name="description" content={config.siteDescription} />
       <link rel="shortcut icon" type="image/png" href={favicon} />
-      <html lang="en" />
     </Helmet>
     <div className={container}>
       <div className={content}>
