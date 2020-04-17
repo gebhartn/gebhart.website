7b66aa22518dba5c995ce0d633120ca634156556
gebhartn <nicholas.gebhart@gmail.com>
2020-04-16 18:48:45 -0700

    feat: opengraph tags

---
+++ src/components/SEO/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import config from '../../../data/SiteConfig'
+import favicon from '../../images/favicon.webp'
+
+export default () => (
+  <Helmet>
+    <meta name="description" content={config.siteDescription} />
+    <meta name="image" content={favicon} />
+  </Helmet>
+)
