e0014677d32f4da563f52e1e0580c9dbb974e8aa
gebhartn <nicholas.gebhart@gmail.com>
2020-05-01 04:56:25 -0700

    feat: adds card

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Img from 'gatsby-image'
+
+import { container, outer, wrapper, content } from './card.module.scss'
+
+export default ({ cards }) => (
+  <section className="projects">
+    {cards.map(({ node: { frontmatter, html } }) => {
+      const { title, path, featuredImage } = frontmatter
+      return (
+        <section id={path} className={outer} key={title}>
+          <div className={wrapper}>
+            <Img fixed={featuredImage.childImageSharp.fixed} alt={title} />
+          </div>
+          <div className={container}>
+            <Link to={path}>
+              <h3>{title}</h3>
+            </Link>
+            <div
+              className={content}
+              dangerouslySetInnerHTML={{ __html: html }}
+            />
+          </div>
+        </section>
+      )
+    })}
+  </section>
+)
