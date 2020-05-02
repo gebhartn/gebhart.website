caf735e8c1e49e211e6ecf8bcdb0b6d16e7aeb73
gebhartn <nicholas.gebhart@gmail.com>
2020-05-02 00:32:57 -0700

    chore: og/twitter meta

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -1,11 +1,24 @@
 import * as React from 'react'
+import { useLocation } from '@reach/router'
 import Helmet from 'react-helmet'
 import config from '../../../data/SiteConfig'
-import favicon from '../../images/favicon.webp'
+import favicon from '../../images/favicon-32x32.png'
 
-export default () => (
-  <Helmet>
-    <meta name="description" content={config.siteDescription} />
-    <meta name="image" content={favicon} />
-  </Helmet>
-)
+export default () => {
+  const { pathname } = useLocation()
+  const url = config.siteUrl + pathname
+
+  return (
+    <Helmet>
+      <meta name="description" content={config.siteDescription} />
+      <meta name="image" content={favicon} />
+
+      <meta property="og:url" content={url} />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={config.twitter} />
+      <meta name="twitter:title" content={config.title} />
+      <meta name="twitter:description" content={config.siteDescription} />
+    </Helmet>
+  )
+}
