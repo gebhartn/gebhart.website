104e0dce674f0631c0205565bcbf1d59c9f68fbc
gebhartn <nicholas.gebhart@gmail.com>
2020-05-02 00:32:34 -0700

    chore: updates favicons

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import * as React from 'react'
 import Helmet from 'react-helmet'
 import SEO from '../components/SEO'
 import Navigation from '../components/Navigation'
 import Footer from '../components/Footer'
 import config from '../../data/SiteConfig'
-import favicon from '../images/favicon.webp'
 import { container, content } from './layout.module.scss'
+import appletouch from '../images/apple-touch-icon.png'
+import thirtytwo from '../images/favicon-32x32.png'
+import sixteen from '../images/favicon-16x16.png'
 
 export default ({ children }) => (
   <>
@@ -13,7 +15,9 @@ export default ({ children }) => (
     <Helmet>
       <html lang="en" />
       <meta name="description" content={config.siteDescription} />
-      <link rel="shortcut icon" type="image/png" href={favicon} />
+      <link rel="apple-touch-icon" sizes="180x180" href={appletouch} />
+      <link rel="icon" type="image/png" sizes="32x32" href={thirtytwo} />
+      <link rel="icon" type="image/png" sizes="16x16" href={sixteen} />
     </Helmet>
     <div className={container}>
       <div className={content}>
